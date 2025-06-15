2716fde76c3323414ef5f43a6b77e438df7e8303
erykszczesniak <eryks453@gmail.com>
2025-06-15 14:10:22 +0200

    fixed formatting

--- Site/src/components/ui/NavLink.jsx
+++ Site/src/components/ui/NavLink.jsx
@@ -1,18 +1,18 @@
-import { Link } from "react-scroll"
+import { Link } from "react-scroll";
 
-const NavLink = ({link, className, onClick, activeClass, offset}) => (
-  <Link 
-    smooth={true} 
-    duration={300} 
-    spy={true} 
-    to={link.id} 
-    offset={offset} 
-    onClick={onClick} 
-    className={className} 
+const NavLink = ({ link, className, onClick, activeClass, offset }) => (
+  <Link
+    smooth={true}
+    duration={300}
+    spy={true}
+    to={link.id}
+    offset={offset}
+    onClick={onClick}
+    className={className}
     activeClass={activeClass}
   >
     {link.link}
   </Link>
-)
+);
 
-export default NavLink
+export default NavLink;
