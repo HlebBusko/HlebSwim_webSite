9ba832d70480794d0956a890c875e2a60f23c7a4
Hleb <hleb>
2025-07-27 19:42:48 +0200

    Refactor the component to support current and second months, add the name input feature, update the ui/ux

--- Site/src/components/layout/Booking4.jsx
+++ Site/src/components/layout/Booking4.jsx
@@ -4,9 +4,12 @@ import axios from 'axios';
 
 function Booking4() {
   const today = dayjs();
-  let startDay = mondayFirst(today.startOf('month').day());
-  let endDay = mondayFirst(today.endOf('month').day());
-  const daysInMonth = today.daysInMonth();
+  const currentMonth = dayjs().startOf('month').month();
+  const [pickedMonth, setPickedMonth] = useState(currentMonth);
+  const selectedMonthObj = dayjs().month(Number(pickedMonth)).startOf('month');
+  let startDay = mondayFirst(selectedMonthObj.day());
+  let endDay = mondayFirst(selectedMonthObj.endOf('month').day());
+  const daysInMonth = selectedMonthObj.daysInMonth();
   const elements = Array.from({ length: daysInMonth }, (_, i) => i + 1);
   const startEmptyBoxes = Array.from({ length: startDay - 1 });
   const endEmptyBoxes = Array.from({ length: 7 - endDay });
@@ -20,34 +23,58 @@ function Booking4() {
     date: day,
     slots: [...timeSlots],
   }));
-  const currentMonth = today.startOf('month').month();
-  const nextMonth = today.add(1, 'month').startOf('month').month();
+
+  const nextMonth = dayjs().add(1, 'month').startOf('month').month();
 
   const [pickedDay, setPickedDay] = useState(null);
   const [displaySlots, setDisplaySlots] = useState(false);
   const [pickedTimeDay, setPickedTimeDay] = useState(null);
   const [bookedLessons, setBookedLessons] = useState(null);
-  const [pickedMonth, setPickedMonth] = useState(currentMonth);
+  const [name, setName] = useState(null);
+  const selectedMonth = Number(pickedMonth);
 
   function handlePickedTimeDay(day, slot) {
-    setPickedTimeDay({ day: day, time: slot });
+    setPickedTimeDay({
+      day: day,
+      time: slot,
+      month: Number(pickedMonth),
+    });
     setDisplaySlots(false);
   }
 
   function handlePickDay(date) {
+    const selectedDate = dayjs().set('date', date).set('month', selectedMonth);
     if (pickedDay === date && displaySlots) {
       setDisplaySlots(false);
+      setPickedDay(null);
       return;
     }
-    if (date >= today.date()) {
+    if (
+      selectedDate.isAfter(today, 'day') ||
+      selectedDate.isSame(today, 'day')
+    ) {
       setPickedDay(date);
       setDisplaySlots(true);
     }
   }
 
+  function isSelectable(date) {
+    const selectedDate = dayjs().set('date', date).set('month', selectedMonth);
+    return (
+      selectedDate.isSame(today, 'day') || selectedDate.isAfter(today, 'day')
+    );
+  }
+
   function handlePickedMonth(e) {
     const { value } = e.target;
     setPickedMonth(value);
+    setPickedTimeDay(null);
+    setPickedDay(null);
+  }
+
+  function handleName(e) {
+    const { value } = e.target;
+    setName(value);
   }
 
   function mondayFirst(day) {
@@ -55,9 +82,14 @@ function Booking4() {
   }
 
   async function sendDataToBackend() {
-    if (!pickedTimeDay) return;
+    if (!pickedTimeDay || !name) return;
+    const newDay = {
+      ...pickedTimeDay,
+      name: name,
+    };
+
     try {
-      await axios.post('http://localhost:3000/bookings', pickedTimeDay);
+      await axios.post('http://localhost:3000/bookings', newDay);
       fetchData();
     } catch (error) {
       console.log(error);
@@ -69,7 +101,12 @@ function Booking4() {
       const bookedLessons = await axios.get('http://localhost:3000/bookings');
       console.log(bookedLessons.data);
       setBookedLessons(
-        bookedLessons.data.map((day) => ({ day: day.day, time: day.time }))
+        bookedLessons.data.map((lesson) => ({
+          day: lesson.day,
+          time: lesson.time,
+          month: lesson.month,
+          name: name,
+        }))
       );
     } catch (error) {
       console.log(error);
@@ -79,107 +116,128 @@ function Booking4() {
     fetchData();
   }, []);
   useEffect(() => {
-    console.log(currentMonth);
-    console.log(nextMonth);
-    console.log('Picked month', pickedMonth);
-  }, [currentMonth, nextMonth, pickedMonth]);
+    console.log(name);
+  }, [name]);
   return (
-    <div className="flex">
-      <div>
-        <div className="grid grid-cols-7 w-[350px]">
-          {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map((day) => (
-            <div
-              key={day}
-              className={`${day === 'Sat' || day === 'Sun' ? 'text-red-500 border-black' : ''} text-center font-semibold`}
-            >
-              {day}
-            </div>
-          ))}
+    <div className="px-8 flex flex-col justify-center items-center bg-bgSection py-4">
+      <div className="font-bold text-2xl">Book your lesson</div>
+      <div className="flex mb-4">
+        <div className="mr-4 w-full">
+          <h1 className="text-xl text-center w-full font-semibold mb-1">
+            Choose the month
+          </h1>
+          <select
+            className="w-full border h-12 py-2 px-4 rounded-xl"
+            value={pickedMonth}
+            onChange={(e) => handlePickedMonth(e)}
+            name=""
+            id=""
+          >
+            <option value={currentMonth}>
+              {today.startOf('month').format('MMMM')}
+            </option>
+            <option value={nextMonth}>
+              {today.startOf('month').add(1, 'month').format('MMMM')}
+            </option>
+          </select>
         </div>
-        <div className="grid grid-cols-7 w-[350px] h-[250px]">
-          {startEmptyBoxes.map((box, i) => (
-            <div className="py-2 text-center " key={`start-${i}`}>
-              {box}
-            </div>
-          ))}
-          {mergedArray.map((item) => {
-            const todayBookedLessons = bookedLessons
-              ? bookedLessons
-                  .filter((lesson) => lesson.day === item.date)
-                  .map((lesson) => lesson.time)
-              : [];
-            return (
-              <div key={item.date}>
-                <div
-                  onClick={() => {
-                    item.date >= today.date() && handlePickDay(item.date);
-                  }}
-                  className={`relative flex justify-center items-center rounded-[50%] w-[100%] h-11 ${item.date === today.date() ? 'font-bold' : ''}  ${pickedDay === item.date ? 'bg-blue-700 text-white font-semibold' : ''}  text-center ${item.date < today.date() ? 'text-gray-500' : 'cursor-pointer'} transition-all duration-200 ease-in-out`}
-                >
-                  {item.date}
+        <div className="flex justify-center items-end gap-4">
+          <input
+            type="text"
+            className="border h-13 rounded-xl pl-4"
+            placeholder="Your name"
+            onChange={(e) => handleName(e)}
+          />
+          <div
+            className={`${pickedTimeDay ? 'px-4' : 'w-30'} px-6 h-12 rounded-xl items-center justify-center border flex gap-4`}
+          >
+            {pickedTimeDay && pickedTimeDay.day}
+            {pickedTimeDay && `.${selectedMonthObj.format('MM')}`}
+            <div>{pickedTimeDay && pickedTimeDay.time}</div>
+          </div>
+          <button
+            onClick={() => {
+              if (pickedTimeDay && name) {
+                sendDataToBackend();
+              }
+            }}
+            className={`h-13 py-2 px-6 rounded-xl  ${pickedTimeDay && name ? 'bg-blue-500 text-white font-semibold hover:bg-blue-600 cursor-pointer hover:shadow-lg' : 'bg-gray-200 shadow'}`}
+          >
+            Book
+          </button>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-7 w-full sm:w-[350px]">
+        {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map((day) => (
+          <div
+            key={day}
+            className={`${day === 'Sat' || day === 'Sun' ? 'text-red-500 border-black' : ''} text-center font-semibold`}
+          >
+            {day}
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-7 w-full sm:w-[350px] sm:h-[250px]">
+        {startEmptyBoxes.map((box, i) => (
+          <div className="py-2 text-center " key={`start-${i}`}>
+            {box}
+          </div>
+        ))}
+        {mergedArray.map((item) => {
+          const todayBookedLessons = bookedLessons
+            ? bookedLessons
+                .filter(
+                  (lesson) =>
+                    lesson.day === item.date &&
+                    Number(lesson.month) === Number(pickedMonth)
+                )
+                .map((lesson) => lesson.time)
+            : [];
+          return (
+            <div key={item.date}>
+              <div
+                onClick={() => {
+                  isSelectable(item.date) && handlePickDay(item.date);
+                }}
+                className={`relative flex justify-center items-center rounded-[50%] w-[100%] h-11 ${item.date === today.date() && pickedMonth === currentMonth ? 'font-bold' : ''}  ${pickedDay === item.date ? 'bg-blue-700 text-white font-semibold' : ''}  text-center ${isSelectable(item.date) ? 'cursor-pointer' : 'text-gray-500 cursor-not-allowed'} transition-all duration-200 ease-in-out`}
+              >
+                {item.date}
+              </div>
+              <div
+                className={`absolute lg:left-100 sm:right-50 right-0 bg-blue-100 z-30 py-2 px-4  ${displaySlots && item.date === pickedDay ? 'block' : 'hidden'}`}
+              >
+                <div className="text-center font-semibold">
+                  Pick available time
                 </div>
-                <div
-                  className={`absolute left-0 bg-blue-100 z-30 py-2 px-4 bot-0 ${displaySlots && item.date === pickedDay ? 'block' : 'hidden'}`}
-                >
-                  <div className="text-center font-semibold">
-                    Pick available time
-                  </div>
-                  <div className="grid grid-cols-6">
-                    {item.slots.map((slot, i) => {
-                      const isBooked = todayBookedLessons.includes(slot);
-                      return (
+                <div className="grid grid-cols-3 md:grid-cols-6">
+                  {item.slots.map((slot, i) => {
+                    const isBooked = todayBookedLessons.includes(slot);
+                    return (
+                      <div
+                        key={`slot-${i}`}
+                        onClick={() =>
+                          !isBooked && handlePickedTimeDay(item.date, slot)
+                        }
+                      >
                         <div
-                          key={`slot-${i}`}
-                          onClick={() =>
-                            !isBooked && handlePickedTimeDay(item.date, slot)
-                          }
+                          className={`py-2 px-2 flex justify-center items-center rounded-lg ${isBooked ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'hover:bg-blue-400 hover:text-white cursor-pointer'}`}
                         >
-                          <div
-                            className={`py-2 px-2  rounded-lg ${isBooked ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'hover:bg-blue-400 hover:text-white cursor-pointer'}`}
-                          >
-                            {slot}
-                          </div>
+                          {slot}
                         </div>
-                      );
-                    })}
-                  </div>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
-            );
-          })}
-          {endEmptyBoxes.map((box, i) => (
-            <div className="py-2 text-center" key={`end-${i}`}>
-              {box}
             </div>
-          ))}
-        </div>
-      </div>
-      <div>
-        <p className="w-30 h-10 border">
-          {pickedTimeDay && pickedTimeDay.day}{' '}
-          {pickedTimeDay && pickedTimeDay.time}
-        </p>
-        <button
-          onClick={pickedTimeDay && sendDataToBackend}
-          className={`border py-2 px-4 rounded-xl  ${pickedTimeDay ? 'bg-blue-500 text-white font-semibold hover:bg-blue-600 cursor-pointer' : 'bg-gray-200'}`}
-        >
-          Book the lesson
-        </button>
-      </div>
-      <div>
-        <select
-          value={pickedMonth}
-          onChange={(e) => handlePickedMonth(e)}
-          name=""
-          id=""
-        >
-          <option value={currentMonth}>
-            {today.startOf('month').format('MMMM')}
-          </option>
-          <option value={nextMonth}>
-            {today.startOf('month').add(1, 'month').format('MMMM')}
-          </option>
-        </select>
+          );
+        })}
+        {endEmptyBoxes.map((box, i) => (
+          <div className="py-2 text-center" key={`end-${i}`}>
+            {box}
+          </div>
+        ))}
       </div>
     </div>
   );
